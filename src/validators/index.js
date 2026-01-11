0e6f24a44a11d22deb7f2454cfb5f6126a78fd1f
snippetarnab <arnabvivo2020@gmail.com>
2026-01-11 11:36:36 +0530

    Add the validator middleware in the codebase.

---
+++ src/validators/index.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email format"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be lowercase")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters long"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
